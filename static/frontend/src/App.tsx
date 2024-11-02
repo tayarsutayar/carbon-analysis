ce64189aceafa01f994635c785425024bc91a7f8
Achmadaniar Anindya Rhosady <tayarsutayar@Achmadaniar-Anindya-Rhosady.local>
2024-11-02 21:22:03 +0700

    Add information

--- static/frontend/src/App.tsx
+++ static/frontend/src/App.tsx
@@ -153,9 +153,11 @@ const App = () => {
         </div>}
 
         {result.output_path == '' && <div className="w-[550px] h-[600px] z-20 bg-stone-100 rounded-xl p-2 shadow-lg border mt-8 flex items-center">
-          <button onClick={analyze} className="bg-yellow-400 px-5 py-3 rounded-lg font-bold mx-auto my-auto" disabled={loading ? true : false}>
+          <span className="text-center w-full">Silahkan cari Area yang anda inginkan kemudian klik analisis untuk mendapatkan potensi nilai karbon
+          <button onClick={analyze} className="w-full bg-yellow-400 px-5 py-3 rounded-lg font-bold mx-auto my-auto" disabled={loading ? true : false}>
             {loading ? 'Analyzing...' : 'Analyze'}
           </button>
+          </span>
         </div>}
       </div>
     </>
